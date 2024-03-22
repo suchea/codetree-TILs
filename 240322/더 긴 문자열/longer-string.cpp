ed07f9775505b4b33788585656b63637ba6963c2
suchea <105552709+suchea@users.noreply.github.com>
2024-03-22 17:00:26 +0900

    Title: 더 긴 문자열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240322/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1, str2;
+    int len1, len2;
+    cin >> str1 >> str2;
+    len1 = str1.length();
+    len2 = str2.length();
+    if(len1 > len2)
+        cout << str1 << " " << len1;
+    else if(len2 > len1)
+        cout << str2 << " " << len2;
+    else
+        cout << "same";
+    return 0;
+}
