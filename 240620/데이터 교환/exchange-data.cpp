3a15f23259bdb37a7d1af957097ab50b55b6a861
suchea <105552709+suchea@users.noreply.github.com>
2024-06-20 19:43:36 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240620/데이터 교환/exchange-data.cpp
+++ 240620/데이터 교환/exchange-data.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 int main() {
-    int a, b, c, arr[3] = {5, 6, 7};
-    b = arr[0];
-    c = arr[1];
-    a = arr[2];
+    int a= 5, b = 6, c = 7, temp;
+    temp = a;
+    a = c;
+    c = b;
+    b = temp;
     printf("%d\n%d\n%d", a, b, c);
     return 0;
 }
