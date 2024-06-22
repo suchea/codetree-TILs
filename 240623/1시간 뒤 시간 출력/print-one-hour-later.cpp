32398100c2c64d2fe3273e531e622bbbed24fac3
suchea <105552709+suchea@users.noreply.github.com>
2024-06-23 01:46:23 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240623/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char c;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cout << a + 1 << ":" << b;
+    return 0;
+}
