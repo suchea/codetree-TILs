255d05e766174c834f70c31eb9aca89820a90b01
suchea <105552709+suchea@users.noreply.github.com>
2024-06-23 00:39:57 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240623/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    char c;
+    cout << fixed;
+    cout.precision(2);
+    cin >> c;
+    cin >> a >> b;
+    cout << c << endl;
+    cout << a << endl;
+    cout << b;
+    return 0;
+}
