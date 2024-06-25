f65d0fffbe6e2fc7bc72cceba57b1538e334ea86
suchea <105552709+suchea@users.noreply.github.com>
2024-06-25 23:32:11 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240625/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a > b) {
+        if(b > c) cout << b;
+        else {
+            if(c > a) cout << a;
+            else cout << c;
+        }
+    }
+    else {
+        if(a > c) cout << a;
+        else {
+            if(c > b) cout << b;
+            else cout << c;
+        }
+    }
+    return 0;
+}
