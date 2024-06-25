0c8967508bacc5f2311cda8a5424aa0fec2f1fb5
suchea <105552709+suchea@users.noreply.github.com>
2024-06-25 16:17:38 +0900

    Title: 두 사람, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240625/두 사람/two-person.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age1, age2;
+    char c1, c2;
+    cin >> age1 >> c1;
+    cin >> age2 >> c2;
+    if((age1 >= 19 || age2 >= 19)&&(c1 == 'M' || c2 == 'M'))
+        cout << 1;
+    else cout << 0;
+    return 0;
+}
