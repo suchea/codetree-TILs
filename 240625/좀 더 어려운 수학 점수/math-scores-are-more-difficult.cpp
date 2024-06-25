49e25b2e96b00c63678a5d5c8fe712679332167b
suchea <105552709+suchea@users.noreply.github.com>
2024-06-25 22:43:29 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240625/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 240625/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -4,11 +4,7 @@ using namespace std;
 int main() {
     int am, ae, bm, be;
     cin >> am >> ae >> bm >> be;
-    if(am > bm)
-        cout << "A";
-    else if(bm > am)
-        cout << "B";
-    else if(ae > be)
+    if(am > bm||(am == bm && ae > be))
         cout << "A";
     else
         cout << "B";
