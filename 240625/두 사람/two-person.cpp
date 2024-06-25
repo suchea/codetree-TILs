f87835350da3b3e85c08cc82dedce4a128d6131a
suchea <105552709+suchea@users.noreply.github.com>
2024-06-25 22:48:44 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240625/두 사람/two-person.cpp
+++ 240625/두 사람/two-person.cpp
@@ -6,7 +6,7 @@ int main() {
     char c1, c2;
     cin >> age1 >> c1;
     cin >> age2 >> c2;
-    if((age1 >= 19 || age2 >= 19)&&(c1 == 'M' || c2 == 'M'))
+    if((age1 >= 19 && c1 == 'M')||(age2 >= 19 && c2 == 'M'))
         cout << 1;
     else cout << 0;
     return 0;
