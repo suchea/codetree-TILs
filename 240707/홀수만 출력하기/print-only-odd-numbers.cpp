9dcb238cf3704e4a7a6d16b053c6ce4334ba632e
suchea <105552709+suchea@users.noreply.github.com>
2024-07-07 20:26:50 +0900

    Title: 홀수만 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240707/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, num;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> num;
+        if(num % 2 == 1 && num % 3 == 0) 
+            cout << num << endl;
+    }
+    return 0;
+}
