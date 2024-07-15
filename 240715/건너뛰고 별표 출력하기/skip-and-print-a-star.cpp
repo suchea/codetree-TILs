7bded4dda9c7515ccd2edea9796def97dcf2e650
suchea <105552709+suchea@users.noreply.github.com>
2024-07-15 23:18:09 +0900

    Title: 건너뛰고 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
+++ 240715/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     cin >> n;
     for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < i + 1; j++)
+        for(int j = 0; j <= i; j++)
             cout << "*";
         cout << endl << endl;
     }
