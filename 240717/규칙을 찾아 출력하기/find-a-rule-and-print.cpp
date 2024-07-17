180d9c6f6bc23a1bd99bf1dc4e4359cdd2c1ccec
suchea <105552709+suchea@users.noreply.github.com>
2024-07-17 16:15:49 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cout << "* ";
+    }
+    cout << endl;
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            cout << "* ";
+        }
+        for(int k = 1; k < n - i; k++) {
+            cout << "  ";
+        }
+        cout << "*" << endl;
+    }
+    return 0;
+}
